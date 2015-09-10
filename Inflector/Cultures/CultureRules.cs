ba09bd824a924a99d51fcb14bb0b9e75dad6df80
Abel Pereira <abel.e.pereira@gmail.com>
2015-09-10 17:54:41 +0100

    Removed unnecessary ctor param

--- Inflector/Cultures/CultureRules.cs
+++ Inflector/Cultures/CultureRules.cs
@@ -2,26 +2,23 @@
 using System.Globalization;
 using JetBrains.Annotations;
 
-namespace Inflector.Rules
+namespace Inflector.Cultures
 {
     public abstract class CultureRules
     {
         private readonly GrammarRules _rules;
 
         protected CultureRules(
-            [NotNull] CultureInfo culture, 
             [NotNull] Func<PluralRules,PluralRules> plurals,
             [NotNull] Func<SingularRules, SingularRules> singulars, 
             [NotNull] Func<IrregularRules, IrregularRules> irregulars, 
             [NotNull] Func<UncountableRules, UncountableRules> uncountables, 
             [NotNull] Func<int, string> ordanizeFunc)
         {
-            if (culture == null) throw new ArgumentNullException(nameof(culture));
             if (plurals == null) throw new ArgumentNullException(nameof(plurals));
             if (singulars == null) throw new ArgumentNullException(nameof(singulars));
             if (uncountables == null) throw new ArgumentNullException(nameof(uncountables));
             if (ordanizeFunc == null) throw new ArgumentNullException(nameof(ordanizeFunc));
-            Culture = culture;
 
             _rules = new GrammarRules();
             _rules.OrdanizeFunc = ordanizeFunc;
@@ -33,6 +30,5 @@ namespace Inflector.Rules
 
         internal InflectorRuleSet RuleSet => _rules.Rules;
 
-        public CultureInfo Culture { get; }
     }
 }
